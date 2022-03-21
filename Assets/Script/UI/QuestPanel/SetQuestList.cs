c5c8631a0d72480974778fbaec4e5bd0af8f79c5
ChangMini <dlwlxns4@gmail.com>
2022-03-21 19:53:06 +0900

    feat : implementation Scroll Quest UI

--- Assets/Script/UI/QuestPanel/SetQuestList.cs
+++ Assets/Script/UI/QuestPanel/SetQuestList.cs
@@ -7,16 +7,25 @@ public class SetQuestList : MonoBehaviour
     private UIChannel uichannel;
     [SerializeField]
     private Text questNameText;
+    [SerializeField]
+    private GameObject questListPrefab;
+    [SerializeField]
+    private GameObject questListPanel;
+    private GridLayoutGroup gridLayout;
 
     private void Awake() 
     {
         uichannel.OnSetQuestOnUI += SetOnQuestList;
         this.gameObject.SetActive(false);
+        gridLayout = GetComponentInChildren<GridLayoutGroup>();
     }
 
     private void SetOnQuestList(string questName)
     {
-        questNameText.text = questName;
+        GameObject questList = Instantiate(questListPrefab, Vector3.zero, Quaternion.identity);
+        questList.GetComponentInChildren<Text>().text = questName;
+
+        questList.transform.SetParent(gridLayout.transform, false);
     }
 
 }
